9332388241172b105b215515c3a7533295a855ec
AlessandroCanto <90458639+AlessandroCanto@users.noreply.github.com>
2022-10-01 22:52:52 -0400

    Update First_Window.java

--- First_Window.java
+++ First_Window.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 
 public class Window {
+    //main method
     public static void main(String[] args) {
-
+        //creates the window object and a title is assigned
         JFrame frame = new JFrame("hello world");
-        //closes the app
+        //stops running the code
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //sets the dimensions of the window
         frame.setSize(800,500);
